6eeaccef3e5ceb14052bf92f3baa9d42025db60d
Mike Sherwood <coolhand2@gmail.com>
2018-06-23 17:04:06 -0600

    Adding uuid, and multiple routes to test out things.

--- index.js
+++ index.js
@@ -1,39 +1,83 @@
 const express = require('express')
-const fs = require('fs');
+const credentials = {};
+const app = express(credentials);
+app.use(express.json());
+
+const uuid = require('uuid/v5');
+const NAMESPACE = uuid('todo-api.shiftedhelix.com', uuid.URL);
+
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-west-2'});
 
-const db = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-const credentials = {};
+const db = new AWS.DynamoDB.DocumentClient();
+const TODO_TABLE = 'todo_demo';
 
-const app = express(credentials);
-
-const cb = (err, data) => {
-    if(err) {
-        return err;
-    }
-    return data;
-}
-
-app.get('/', (req, res) => {
-    console.log("Retrieving all tables as listing");
-    db.listTables({},(err, data) => {
-        if(err) { res.send(err); }
-        else{ res.send(data); }
+app.get('/item', (req, res) => {
+    const params = {
+        TableName: TODO_TABLE,
+        ProjectionExpression: "id, done, todo"
+    };
+    db.scan(params, (err, data) => {
+        if(err){ res.send({success: false, error: err}); }
+        else{ res.send({success: true, data: data}); }
     });
 });
 app.get('/item/:id', (req, res) => {
-    console.log("Fetching by id");
-    console.log("DEBUG: parameters: " + req.params);
-    console.log("DEBUG: id?: " + req.params.id);
+    console.log("Fetching by id [" + req.params.id + "]");
     const params = {
-        TableName: 'todo_demo',
-        Key: {'id': {N: req.params.id}}
+        TableName: TODO_TABLE,
+        Key: {'id': {S: req.params.id}}
     };
-    db.getItem(params,(err, data) => {
-        if(err) { res.send(err); }
-        else{ res.send(data); }
+    db.get(params,(err, data) => {
+        if(err) { res.send({success: false, error: err}); }
+        else{ res.send({success: true, data: data}); }
     });
 });
+app.put('/item', (req, res) => {
+    const newId = uuid(req.body, NAMESPACE);
+    const params = {
+        TableName: TODO_TABLE, 
+        Item: {
+            id: newId,
+            done: {BOOL: req.body.done},
+            todo: {S: req.body.todo}
+        }
+    };
+    db.put(params, (err, data) => {
+        if(err) { res.send({success: false, error: err}); }
+        else{ res.send({success: true, data: newId}); }
+    });
+});
+app.post('/item/:id', (req, res) => {
+    const params = {
+        TableName: TODO_TABLE,
+        Key: {
+            id: {N: req.body.id},
+        },
+        UpdateExpression: 'set done = :d, todo = :t',
+        ExpressionValues: {
+            ':d': req.body.done,
+            ':t': req.body.todo
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+    db.update(params, (err, data) => {
+        if(err){ res.send({success: false, error: err}); }
+        else{ res.send({success: true}); }
+    });
+});
+app.delete('/item/:id', (req, res) => {
+    const params = {
+        TableName: TODO_TABLE,
+        Key: {
+            "id": req.body.id
+        }
+    };
+    db.delete(params, (err, data) => {
+        if(err){ res.send({success: false, error: err}); }
+        else{ res.send({success: true, data: data}); }
+    });
+});
+
 
 app.listen(3000);
