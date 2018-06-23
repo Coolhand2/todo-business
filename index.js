2d9745e121f8df18e719b2b63c9e2e6192f311e5
Mike Sherwood <coolhand2@gmail.com>
2018-06-23 15:11:14 -0600

    We have access! Now lets show some stuff.

--- index.js
+++ index.js
@@ -4,24 +4,16 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-west-2'});
 
 const db = new AWS.DynamoDB({apiVersion: '2012-10-08'});
-
-const params = {
-    TableName: "",
-    Key: {
-        'KEY_NAME': {S: 'id'}
-    }
-};
-
-//var privateKey = fs.readFileSync('/opt/data/server.key');
-//var certificate = fs.readFileSync('/opt/data/server.crt');
-
-//var credentials = {key: privateKey, cert: certificate};
 const credentials = {};
 
 const app = express(credentials);
 
 app.get('/', function(req, res) {
-    db.listTables({Limit: 10}, function(err, data){
+    const params = {
+        TableName: 'todo_demo',
+        Key: {'id': {N: '0'}}
+    };
+    db.getItem(params, function(err, data){
         if(err) {
             res.send(err);
         } else {
